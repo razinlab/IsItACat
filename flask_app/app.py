743e34f715a1a29df3287cdcecd38525f33b1817
razinlab <razin.ahsab@baruchmail.cuny.edu>
2025-02-24 20:45:20 -0500

    Update app.py

--- flask_app/app.py
+++ flask_app/app.py
@@ -25,5 +25,5 @@ def predict():
     return jsonify({'result': result, 'confidence': confidence})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=10000)
 
