76af130cdd8626103218819ce390ace60c44d5c7
razinlab <razin.ahsab@baruchmail.cuny.edu>
2025-02-24 20:06:46 -0500

    Update app.py

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -13,13 +13,9 @@ if file is not None:
     img_bytes = file.getvalue()
     file = {'file': img_bytes}
     response = requests.post('https://isitacatapi.onrender.com/predict', files=file)
-
-    if response.status_code == 200:
-        result = response.json()['result']
-        confidence = response.json()['confidence']
-        if result == 'cat':
-            st.write(f'This is a {result} with {confidence*100:.2f}% confidence.')
-        elif result == 'not a cat':
-            st.write(f'This is {result} with {confidence*100:.2f}% confidence.')
-    else:
-        st.write('Something went wrong.')
+    result = response.json()['result']
+    confidence = response.json()['confidence']
+    if result == 'cat':
+        st.write(f'This is a {result} with {confidence*100:.2f}% confidence.')
+    elif result == 'not a cat':
+        st.write(f'This is {result} with {confidence*100:.2f}% confidence.')
