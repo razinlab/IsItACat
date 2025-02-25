61682e2853921d9d5608f803864604d3f6f75c21
razinlab <razin.ahsab@baruchmail.cuny.edu>
2025-02-24 20:56:55 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 cd flask_app
-gunicorn -w 1 -b 0.0.0.0:5000 app:app &
+gunicorn -w 1 -b 0.0.0.0:10000 app:app &
 
 cd streamlit_app
 streamlit run app.py --server.port 10000 --server.address 0.0.0.0
